c155650596fe1a3540fe2dcbc86f97c900b69a5b
Andrey Nuykin <93701808+cruelper@users.noreply.github.com>
2022-11-02 10:10:59 +0400

    origin

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { AppProvider } from '@edx/frontend-platform/react';
 
+import Footer from '@edx/frontend-component-footer';
+import Header from '@edx/frontend-component-header';
+
 import { routePath } from 'data/constants/app';
 import store from 'data/store';
 import GradebookPage from 'containers/GradebookPage';
